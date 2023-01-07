7f99a9c3e2d505ffd5fa6519ce5d642f1f211924
Tess von Rueden <tessvonr@umich.edu>
2023-01-06 20:19:10 -0500

    Moving over website files

---
+++ js/page.js
@@ -0,0 +1,9 @@
+// Main
+function showInfo() {
+    document.getElementById("rover").src = "images/BLiSS_Rover2.png";
+}
+
+function hideInfo() {
+    document.getElementById("roverbox").style.visibility = 'hidden';
+    document.getElementById("rover").src = "images/BLiSS_Rover.png";
+}
